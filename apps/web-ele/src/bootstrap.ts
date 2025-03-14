76387c3e338ff996474fbd8d9cb5fb6dbb9ac0ef
suYan <709936456@qq.com>
2025-03-14 16:29:50 +0800

    refactor-只注册iconify的Material Design Icons图标，按需导入，离线状态下图标可以显示

--- apps/web-ele/src/bootstrap.ts
+++ apps/web-ele/src/bootstrap.ts
@@ -9,6 +9,8 @@ import '@vben/styles';
 import '@vben/styles/ele';
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import mdi from '@iconify-json/mdi/icons.json';
+import { addCollection } from '@iconify/vue';
 import { useTitle } from '@vueuse/core';
 import ElementPlus, { ElLoading } from 'element-plus';
 
@@ -57,6 +59,14 @@ async function bootstrap(namespace: string) {
 
   // 全局引入elementPlus
   app.use(ElementPlus);
+
+  /**
+   * 1注册vue的iconify: pnpm install --save-dev @iconify/vue
+   * 2按需导入icon插件: pnpm i -D unplugin-icons
+   * 3添加Material Design Icons本地图标，离线可以使用: pnpm add -D @iconify-json/mdi
+   *
+   */
+  addCollection(mdi);
 
   // 注册所有elementPlus图标
   for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
--- apps/web-ele/vite.config.mts
+++ apps/web-ele/vite.config.mts
@@ -10,6 +10,7 @@ export default defineConfig(async () => {
         ElementPlus({
           format: 'esm',
         }),
+
         Icons({
           autoInstall: true,
         }),
