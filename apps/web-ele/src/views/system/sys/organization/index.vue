295da15e8b63dfaf1ef865a892332160dcad1464
suYan <709936456@qq.com>
2025-04-27 15:53:31 +0800

    refactor-优化机构管理页面

--- apps/web-ele/src/views/system/sys/organization/index.vue
+++ apps/web-ele/src/views/system/sys/organization/index.vue
@@ -5,7 +5,7 @@ import type {
   OrganizationVO,
 } from '#/api/system/sys/organiation';
 
-import { computed, onMounted, reactive, ref, watch } from 'vue';
+import { onMounted, reactive, ref, watch } from 'vue';
 
 import { ElForm, ElMessage, ElMessageBox, ElTree } from 'element-plus';
 
@@ -56,21 +56,12 @@ const formData = reactive<OrganizationForm>({
   status: 1,
 });
 
-const orgTypeLabel = computed(() => {
-  return formData.organType === 0 ? '机构名称' : '部门名称';
-});
-
 const rules = reactive({
   parentId: [{ required: true, message: '上级机构不能为空', trigger: 'blur' }],
-  organName: [{ required: true, message: '机构名称不能为空', trigger: 'blur' }],
-  organType: [{ required: true, message: '机构类型不能为空', trigger: 'blur' }],
+  organName: [{ required: true, message: '名称不能为空', trigger: 'blur' }],
+  organCode: [{ required: true, message: '编码不能为空', trigger: 'blur' }],
+  organType: [{ required: true, message: '类型不能为空', trigger: 'blur' }],
 });
-
-// 机构类型下拉选项
-const options = [
-  { value: 1, label: '机构' },
-  { value: 2, label: '部门' },
-];
 
 // 新增或编辑机构，弹出窗
 const dialog = reactive({
@@ -104,17 +95,17 @@ function resetQuery() {
 /**
  * 新增或编辑机构，打开弹出窗
  */
-async function handleOpenDialog(parentId?: string, id?: string) {
+async function handleOpenDialog(id?: string, parentId?: string) {
   dialog.visiable = true;
   // 组织下拉树
   await OrgOptionTree();
 
   if (id) {
-    dialog.title = '修改机构';
+    dialog.title = '修改';
     const data = await getOrgDetailApi(id);
     Object.assign(formData, data);
   } else {
-    dialog.title = '新增机构';
+    dialog.title = '新增';
     formData.parentId = parentId;
   }
 }
@@ -123,45 +114,31 @@ async function handleOpenDialog(parentId?: string, id?: string) {
  * 新增或编辑机构，关闭弹出窗
  */
 function handleCloseDialog() {
-  dialog.visiable = false;
   orgFormRef.value.resetFields();
   orgFormRef.value.clearValidate();
+  dialog.visiable = false;
 }
 
 /**
  * 提交表单
  * @param deptId
  */
-function handleSubmit() {
-  orgFormRef.value.validate((valid: boolean) => {
-    if (valid) {
-      loading.value = true;
-      const id = formData.id;
-      if (id) {
-        editOrgApi(formData)
-          .then(() => {
-            ElMessage.success('修改成功!');
-            handleCloseDialog();
-            resetQuery();
-            OrgOptionTree();
-          })
-          .finally(() => {
-            loading.value = false;
-          });
-      } else {
-        addOrgApi(formData)
-          .then(() => {
-            ElMessage.success('新增成功!');
-            handleCloseDialog();
-            resetQuery();
-            OrgOptionTree();
-          })
-          .finally(() => {
-            loading.value = false;
-          });
-      }
-    }
-  });
+async function handleSubmit() {
+  const valid = orgFormRef.value.validate();
+  if (!valid) return;
+
+  loading.value = true;
+
+  try {
+    const id = formData.id;
+    await (id ? editOrgApi(formData) : addOrgApi(formData));
+    ElMessage.success(id ? '修改成功！' : '新增成功');
+    handleCloseDialog();
+    resetQuery();
+    await OrgOptionTree();
+  } finally {
+    loading.value = false;
+  }
 }
 
 /**
@@ -192,9 +169,7 @@ function handleDelete(id?: string) {
  * 组织机构部门下拉树
  */
 async function OrgOptionTree() {
-  const data = await orgOptionTreeApi();
-  organTreeOptionData.value = data;
-  organTreeOptionData.value = [{ value: '0', label: '机构树', children: data }];
+  organTreeOptionData.value = await orgOptionTreeApi();
 }
 
 onMounted(() => {
@@ -204,83 +179,78 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="app-container">
-    <el-row>
-      <!-- 部门树 -->
-      <el-col :lg="4" :xs="24">
-        <el-card shadow="never">
+  <div class="flex h-full gap-2 p-2">
+    <!-- 部门树 -->
+    <el-card class="w-1/4">
+      <el-input v-model="organName" style="width: 240px" placeholder="机构名称">
+        <template #prefix>
+          <el-icon class="el-input__icon"><search /></el-icon>
+        </template>
+      </el-input>
+
+      <ElTree
+        class="mt-5"
+        ref="organTreeRef"
+        :data="organTreeOptionData"
+        highlight-current
+        :default-expand-all="true"
+        :filter-node-method="filterNode"
+      />
+    </el-card>
+
+    <el-card class="w-full">
+      <ElForm
+        ref="queryElFormRef"
+        :inline="true"
+        :model="queryParams"
+        class="mb-2"
+      >
+        <el-form-item prop="organName">
           <el-input
-            v-model="organName"
-            style="width: 240px"
             placeholder="机构名称"
-          >
-            <template #prefix>
-              <el-icon class="el-input__icon"><search /></el-icon>
-            </template>
-          </el-input>
-
-          <ElTree
-            class="mt-5"
-            ref="organTreeRef"
-            :data="organTreeOptionData"
-            highlight-current
-            :default-expand-all="true"
-            :filter-node-method="filterNode"
+            clearable
+            v-model="queryParams.keyWord"
+            @keyup.enter="handleQuery()"
           />
-        </el-card>
-      </el-col>
+        </el-form-item>
 
-      <el-col :lg="20" :xs="24">
-        <div class="search-container">
-          <ElForm ref="queryElFormRef" :inline="true" :model="queryParams">
-            <el-form-item prop="organName">
-              <el-input
-                placeholder="机构名称"
-                clearable
-                v-model="queryParams.keyWord"
-                @keyup.enter="handleQuery()"
-              />
-            </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="handleQuery()">
+            <template #icon>
+              <el-icon><Search /></el-icon>
+            </template>
+            搜索
+          </el-button>
 
-            <el-form-item>
-              <el-button type="primary" @click="handleQuery()">
-                <template #icon>
-                  <el-icon><Search /></el-icon>
-                </template>
-                搜索
-              </el-button>
+          <el-button type="primary" @click="resetQuery()">
+            <template #icon>
+              <el-icon><Refresh /></el-icon>
+            </template>
+            重置
+          </el-button>
 
-              <el-button type="primary" @click="resetQuery()">
-                <template #icon>
-                  <el-icon><Refresh /></el-icon>
-                </template>
-                重置
-              </el-button>
-
-              <el-button
-                type="primary"
-                @click="handleOpenDialog('0')"
-                v-access:code="['sys:organ:add']"
-              >
-                <template #icon>
-                  <el-icon><Plus /></el-icon>
-                </template>
-                新增机构
-              </el-button>
-            </el-form-item>
-          </ElForm>
-        </div>
-
-        <el-card class="table-container">
-          <el-table
-            border
-            :data="OrganTableData"
-            v-loading="loading"
-            default-expand-all
-            row-key="id"
-            highlight-current-row
+          <el-button
+            type="primary"
+            @click="handleOpenDialog()"
+            v-access:code="['sys:organ:add']"
           >
-            <!-- <el-table-column
+            <template #icon>
+              <el-icon><Plus /></el-icon>
+            </template>
+            新增机构
+          </el-button>
+        </el-form-item>
+      </ElForm>
+
+      <el-table
+        border
+        :data="OrganTableData"
+        v-loading="loading"
+        default-expand-all
+        row-key="id"
+        highlight-current-row
+      >
+        <!-- <el-table-column
           v-if="false"
           prop="id"
         />
@@ -295,108 +265,95 @@ onMounted(() => {
           prop="parentId"
         /> -->
 
-            <el-table-column
-              label="名称"
-              prop="organName"
-              align="left"
-              min-width="200"
-            />
+        <el-table-column
+          label="名称"
+          prop="organName"
+          align="left"
+          min-width="200"
+        />
 
-            <el-table-column
-              label="机构类型"
-              prop="organType"
-              align="center"
-              width="100"
+        <el-table-column
+          label="类型"
+          prop="organType"
+          align="center"
+          width="100"
+        >
+          <template #default="scope">
+            <el-tag v-if="scope.row.organType === 1" type="primary">
+              机构
+            </el-tag>
+            <el-tag v-else type="success">部门</el-tag>
+          </template>
+        </el-table-column>
+
+        <el-table-column
+          label="编码"
+          prop="organCode"
+          align="center"
+          width="120"
+        />
+
+        <el-table-column label="排序" prop="sort" align="center" width="80" />
+
+        <el-table-column label="状态" align="center" width="80">
+          <template #default="scope">
+            <el-tag v-if="scope.row.status === 1" type="success"> 显示 </el-tag>
+            <el-tag v-else type="info">隐藏</el-tag>
+          </template>
+        </el-table-column>
+
+        <el-table-column
+          label="创建时间"
+          prop="createTime"
+          align="center"
+          width="200"
+        />
+
+        <el-table-column fixed="right" align="center" label="操作" width="300">
+          <template #default="scope">
+            <el-button
+              v-if="scope.row.organType === 1"
+              type="primary"
+              link
+              size="small"
+              v-access:code="['sys:organ:add']"
+              @click="handleOpenDialog(null, scope.row.parentId)"
             >
-              <template #default="scope">
-                <el-tag v-if="scope.row.organType === 1" type="primary">
-                  机构
-                </el-tag>
-                <el-tag v-else type="warning">部门</el-tag>
-              </template>
-            </el-table-column>
+              <el-icon><Plus /></el-icon>
+              新增
+            </el-button>
 
-            <el-table-column
-              label="机构编码"
-              prop="organCode"
-              align="center"
-              width="120"
-            />
-
-            <el-table-column
-              label="排序"
-              prop="sort"
-              align="center"
-              width="80"
-            />
-
-            <el-table-column label="状态" align="center" width="80">
-              <template #default="scope">
-                <el-tag v-if="scope.row.status === 1" type="success">
-                  显示
-                </el-tag>
-                <el-tag v-else type="info">隐藏</el-tag>
-              </template>
-            </el-table-column>
-
-            <el-table-column
-              label="创建时间"
-              prop="createTime"
-              align="center"
-              width="200"
-            />
-
-            <el-table-column
-              fixed="right"
-              align="center"
-              label="操作"
-              width="300"
+            <el-button
+              type="primary"
+              link
+              size="small"
+              v-access:code="['sys:organ:edit']"
+              @click="handleOpenDialog(scope.row.id, null)"
             >
-              <template #default="scope">
-                <el-button
-                  v-if="scope.row.organType === 1"
-                  type="primary"
-                  link
-                  size="small"
-                  v-access:code="['sys:organ:add']"
-                  @click="handleOpenDialog(scope.row.id)"
-                >
-                  <el-icon><Plus /></el-icon>
-                  新增
-                </el-button>
+              <el-icon><Edit /></el-icon> 编辑
+            </el-button>
 
-                <el-button
-                  type="primary"
-                  link
-                  size="small"
-                  v-access:code="['sys:organ:edit']"
-                  @click="handleOpenDialog(undefined, scope.row.id)"
-                >
-                  <el-icon><Edit /></el-icon> 编辑
-                </el-button>
-
-                <el-button
-                  type="primary"
-                  link
-                  size="small"
-                  v-access:code="['sys:organ:delete']"
-                  @click="handleDelete(scope.row.id)"
-                >
-                  <el-icon><Delete /></el-icon>
-                  删除
-                </el-button>
-              </template>
-            </el-table-column>
-          </el-table>
-        </el-card>
-      </el-col>
-    </el-row>
+            <el-button
+              type="primary"
+              link
+              size="small"
+              v-access:code="['sys:organ:delete']"
+              @click="handleDelete(scope.row.id)"
+            >
+              <el-icon><Delete /></el-icon>
+              删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
 
     <el-dialog
       v-model="dialog.visiable"
       :title="dialog.title"
       :width="dialog.width"
       append-to-body
+      draggable
       center
       @close="handleCloseDialog()"
     >
@@ -406,40 +363,33 @@ onMounted(() => {
         :rules="rules"
         label-width="80px"
       >
-        <el-form-item label="上级机构" prop="parentId">
+        <el-form-item
+          v-if="formData.parentId !== '0'"
+          label="上级机构"
+          prop="parentId"
+        >
           <el-tree-select
             v-model="formData.parentId"
             :data="organTreeOptionData"
             filterable
             check-strictly
-            :render-after-expand="false"
+            :default-expand-all="true"
           />
         </el-form-item>
 
-        <el-form-item prop="organName" :label="orgTypeLabel">
+        <el-form-item prop="organName" label="名称">
           <el-input v-model="formData.organName" placeholder="名称" />
         </el-form-item>
 
-        <!-- <el-form-item label="类型" prop="organType">
+        <el-form-item prop="organCode" label="编码">
+          <el-input v-model="formData.organCode" placeholder="编码" />
+        </el-form-item>
+
+        <el-form-item label="类型" prop="organType">
           <el-radio-group v-model="formData.organType">
             <el-radio :value="1">机构</el-radio>
             <el-radio :value="2">部门</el-radio>
           </el-radio-group>
-        </el-form-item> -->
-
-        <el-form-item label="类型" prop="organType">
-          <el-select
-            v-model="formData.organType"
-            clearable
-            placeholder="机构类型"
-          >
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
-            />
-          </el-select>
         </el-form-item>
 
         <el-form-item label="排序" prop="sort">
